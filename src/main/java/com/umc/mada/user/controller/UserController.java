07c0eb34ff44e8311f09a314146ceb497d66a2f9
jinnxyoung <wlsdud7722@naver.com>
2023-07-17 14:40:43 +0900

    Feat : User 클래스 수정

--- src/main/java/com/umc/mada/user/controller/UserController.java
+++ src/main/java/com/umc/mada/user/controller/UserController.java
@@ -1,4 +1,13 @@
 package com.umc.mada.user.controller;
 
+import io.swagger.annotations.Api;
+import lombok.RequiredArgsConstructor;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RequiredArgsConstructor
+@RestController
+@RequestMapping("/user")
 public class UserController {
+
 }
