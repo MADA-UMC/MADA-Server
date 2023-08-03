6a12d3f8de0da80b59374ba2a7055bbc429659b0
jinnxyoung <wlsdud7722@naver.com>
2023-08-04 04:22:35 +0900

    Feat : 전체 공지사항 조회 기능 구현

--- src/main/java/com/umc/mada/notice/repository/NoticeRepository.java
+++ src/main/java/com/umc/mada/notice/repository/NoticeRepository.java
@@ -1,4 +1,15 @@
 package com.umc.mada.notice.repository;
 
-public interface NoticeRepository {
+import com.umc.mada.notice.domain.Notice;
+import com.umc.mada.notice.dto.NoticeListResponseDto;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+
+@Repository
+public interface NoticeRepository extends JpaRepository<Notice, Long> {
+    @Query("SELECT n FROM Notice n ORDER BY n.id DESC")
+    List<NoticeListResponseDto> findAllDesc();
 }
