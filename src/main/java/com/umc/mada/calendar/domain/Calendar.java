243c556bde5af4788a514492c28429692a3628f1
강지훈 <clone0922@gmail.com>
2023-08-04 13:19:46 +0900

    Update : Calendar Class 수정

--- src/main/java/com/umc/mada/calendar/domain/Calendar.java
+++ src/main/java/com/umc/mada/calendar/domain/Calendar.java
@@ -4,25 +4,33 @@ package com.umc.mada.calendar.domain;
 import com.umc.mada.global.BaseEntity;
 import com.umc.mada.user.domain.User;
 import lombok.*;
+import net.bytebuddy.asm.Advice;
+import org.hibernate.annotations.CreationTimestamp;
+import org.hibernate.annotations.UpdateTimestamp;
 
 import javax.persistence.*;
 import java.sql.Date;
 import java.sql.Timestamp;
+import java.time.LocalDateTime;
 
 
 @Entity
 @Getter
 @Setter
 @Builder
-@NoArgsConstructor
 @AllArgsConstructor
-public class Calendar extends BaseEntity {
+@NoArgsConstructor
+@Table(name = "CALENDER")
+public class Calendar {
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
     @Column(name = "id")
     private Long id;
-    @Column(name = "user_id")
-    private Long uid;
+
+    @ManyToOne
+    @JoinColumn(name = "user_id")
+    private User user;
+
     @Column(name = "calender_name")
     private String calenderName;
     @Column(name = "color")
@@ -31,10 +39,18 @@ public class Calendar extends BaseEntity {
     private Date startDate;
     @Column(name = "end_date")
     private Date endDate;
-    @Column(name = "repeat")
+    @Column(name = "`repeat`")
     private String repeat;
     @Column(name = "d_day")
     private Character d_day;
     @Column(name = "memo")
     private String memo;
+
+    @CreationTimestamp
+    @Column(name = "create_at", updatable = false)
+    private LocalDateTime createdAt; // 생성 시간
+
+    @UpdateTimestamp
+    @Column(name = "update_at")
+    private LocalDateTime updatedAt; // 수정 시간
 }
