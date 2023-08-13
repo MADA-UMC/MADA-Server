11d65ca52702f90af58e3fb26ebce35474f2707a
jinnxyoung <wlsdud7722@naver.com>
2023-08-14 00:24:38 +0900

    Update : Notice return 타입, mapping 수정

--- src/main/java/com/umc/mada/notice/controller/NoticeController.java
+++ src/main/java/com/umc/mada/notice/controller/NoticeController.java
@@ -3,14 +3,22 @@ package com.umc.mada.notice.controller;
 import com.umc.mada.notice.dto.NoticeListResponseDto;
 import com.umc.mada.notice.service.NoticeService;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.ResponseEntity;
+import org.springframework.security.core.Authentication;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @RestController
 @RequiredArgsConstructor
 public class NoticeController {
     private  final NoticeService noticeService;
-    @GetMapping("/notices")
-    public List<NoticeListResponseDto> getAllNotice() { return noticeService.findAllNotice(); }
+    @GetMapping("/allnotice")
+    public ResponseEntity<Map<String, Object>> getAllNotice() {
+        Map<String, Object> map = new HashMap<>();
+        map.put("data", noticeService.findAllNotice());
+        return ResponseEntity.ok(map);
+    }
 }
