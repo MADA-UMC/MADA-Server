2c2b41799d512911e058e041ba7229d8c783d6c7
jeonghee-Han <134862850+Jeonghee-Han@users.noreply.github.com>
2023-08-19 00:25:06 +0900

    Update : Timetable 생성시 일정 불러오기 수정
    -d-day 반환 추가

--- src/main/java/com/umc/mada/timetable/controller/TimetableController.java
+++ src/main/java/com/umc/mada/timetable/controller/TimetableController.java
@@ -125,6 +125,7 @@ public class TimetableController {
                 calendarMap.put("color", calendar.getColor());
                 calendarMap.put("startTime", calendar.getStartTime()); // 시작 시간
                 calendarMap.put("endTime", calendar.getEndTime());     // 종료 시간
+                calendarMap.put("d-day", calendar.getDday());
                 calendarList.add(calendarMap);
             }
         }
