22f55edf5130e492b479ddb61189f1d862a4bea3
jeonghee-Han <134862850+Jeonghee-Han@users.noreply.github.com>
2023-08-21 03:41:13 +0900

    Update : 반복 투두 추가 기능 수정
    -repeatWeek, Month, date : Nullable

--- src/main/java/com/umc/mada/todo/dto/TodoRequestDto.java
+++ src/main/java/com/umc/mada/todo/dto/TodoRequestDto.java
@@ -5,16 +5,21 @@ import com.umc.mada.todo.domain.RepeatMonth;
 import lombok.*;
 import com.umc.mada.todo.domain.Repeat;
 import com.umc.mada.todo.domain.RepeatWeek;
+import reactor.util.annotation.Nullable;
+
 import java.time.LocalDate;
 
 @Data
 public class TodoRequestDto {
+    @Nullable
     private LocalDate date; // 투두 일자
     private Category category; // 카테고리 ID
     private String todoName; // 투두 이름
     private Boolean complete; // 완료 여부 (y: 오늘 할 일 완료, n: 오늘 할 일 미완료)
     private Repeat  repeat; // 반복 설정 (N: 반복 안함, day: 매일 반복, week: 매주 반복, month: 매월 반복)
+    @Nullable
     private RepeatWeek  repeatWeek; // 매주 반복 요일 ("mon", "tue", "wed", "thu", "fri", "sat", "sun")
+    @Nullable
     private RepeatMonth repeatMonth;
     private LocalDate startRepeatDate; // 반복 시작 일자
     private LocalDate endRepeatDate; // 반복 종료 일자
