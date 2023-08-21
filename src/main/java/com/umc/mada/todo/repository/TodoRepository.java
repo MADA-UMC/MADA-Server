95f0eec4cfae597b76b38b86f1e6fad9c9c7d7b3
gollog <clone0922@gmail.com>
2023-08-21 20:55:49 +0900

    Test : TestCommit

--- src/main/java/com/umc/mada/todo/repository/TodoRepository.java
+++ src/main/java/com/umc/mada/todo/repository/TodoRepository.java
@@ -5,9 +5,12 @@ import com.umc.mada.todo.domain.RepeatMonth;
 import com.umc.mada.todo.domain.Todo;
 import com.umc.mada.user.domain.User;
 import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import org.springframework.stereotype.Repository;
 
 import java.time.LocalDate;
+import java.util.Date;
 import java.util.List;
 import java.util.Optional;
 
@@ -17,4 +20,7 @@ public interface TodoRepository extends JpaRepository<Todo, Integer>{
     List<Todo> findTodosByUserIdAndDateIs(User userId, LocalDate date);
     Optional<Todo> deleteTodoByUserIdAndId(User userId, int id);
     Optional<Todo> findTodoByUserIdAndId(User userId, int id);
+
+    @Query("select  AVG(complete) from Todo where repeat = 'N' and endRepeatDate > :endDate and startRepeatDate < :startDate" )
+    List<Todo> findTodosAVG(@Param("endDate") Date endDate,@Param("startDate") Date startDate);
 }
