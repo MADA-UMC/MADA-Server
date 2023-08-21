8e3f579cd936a3b8916e5de26e30543227a2b61a
yujiyea <54650556+yujiyea@users.noreply.github.com>
2023-08-21 18:02:16 +0900

    Feat : 예외처리 구현(custom관련 예외만 추가함)

---
+++ src/main/java/com/umc/mada/exception/ErrorType.java
@@ -0,0 +1,14 @@
+package com.umc.mada.exception;
+
+import lombok.Getter;
+import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpStatus;
+
+@Getter
+@RequiredArgsConstructor
+public enum ErrorType {
+    BUY_OWNED_ITEM_ERROR(HttpStatus.CONFLICT, "이미 소유한 아이템입니다.");
+
+    private final HttpStatus errorCode;
+    private final String message;
+}
