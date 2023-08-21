7f56728f8e2c9a6535ade146e1f4269d49660522
yujiyea <54650556+yujiyea@users.noreply.github.com>
2023-08-21 18:05:04 +0900

    Update : 아이템 구매, 사용자 캐릭터 변경 수정
    - 아이템 구매 : 같은 아이템 구매시 예외해서 response 보내기
    - 사용자 캐릭터 변경 : 이전에 입고 있는 아이템 지우고 새로운 아이템 입히도록 수정

--- src/main/java/com/umc/mada/custom/dto/UserCharacterResponse.java
+++ src/main/java/com/umc/mada/custom/dto/UserCharacterResponse.java
@@ -3,16 +3,15 @@ package com.umc.mada.custom.dto;
 import com.umc.mada.custom.domain.CustomItem;
 import lombok.*;
 
-import java.util.ArrayList;
 import java.util.List;
 import java.util.stream.Collectors;
 
 @Getter
 public class UserCharacterResponse {
-    private final List<CharacterItemResponse> data;
+    private final List<CharacterItemResponse> wearingItems;
 
     public UserCharacterResponse(List<CharacterItemResponse> items){
-        this.data = items;
+        this.wearingItems = items;
     }
 
 //    public UserCharacterResponse(CharacterItemResponse characterItemResponse){
--- src/main/java/com/umc/mada/custom/service/CustomService.java
+++ src/main/java/com/umc/mada/custom/service/CustomService.java
@@ -1,6 +1,5 @@
 package com.umc.mada.custom.service;
 
-import com.amazonaws.services.s3.AmazonS3;
 import com.umc.mada.custom.domain.CustomItem;
 import com.umc.mada.custom.domain.HaveItem;
 import com.umc.mada.custom.domain.ItemType;
@@ -9,6 +8,8 @@ import com.umc.mada.custom.dto.ItemsElementResponse;
 import com.umc.mada.custom.dto.UserCharacterResponse;
 import com.umc.mada.custom.repository.CustomRepository;
 import com.umc.mada.custom.repository.HaveItemRepository;
+import com.umc.mada.exception.BuyOwnedItemException;
+import com.umc.mada.exception.ErrorType;
 import com.umc.mada.user.domain.User;
 import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
@@ -37,7 +38,7 @@ public class CustomService {
         List<CustomItem> colorItems = customItems.stream().filter(item -> ItemType.I1.equals(item.getItemType())).collect(Collectors.toList());
         if(customItems.isEmpty()|| colorItems.isEmpty()){ //사용자 캐릭터가 디폴트 값이라면 디폴트 캐릭터 데이터를 넘겨준다.
             //return new UserCharacterResponse(CharacterItemResponse.of(customRepository.findCustomItemById(1L).get()));
-            customItems.add(customRepository.findCustomItemById(1).get()); //TODO: isPresent() 체크하기
+            customItems.add(customRepository.findCustomItemById(10).get()); //TODO: isPresent() 체크하기
         }
         return UserCharacterResponse.of(customItems);
     }
@@ -69,52 +70,21 @@ public class CustomService {
     }
 
     public void changeUserItem(User user, String[] items_id){//List<String> items_id
+        //이전에 입고 있는 아이템 없애기
+        List<HaveItem> oldhaveItemList = haveItemRepository.findByUserAndWearing(user, true);
+        for(HaveItem oldItem: oldhaveItemList){ //예전에 입은 아이템은 false로 해준다.
+            oldItem.updateHaveItemWearing(false);
+            haveItemRepository.save(oldItem);
+        }
+
+        //새로운 아이템 입히기
         for(String item_id : items_id){
             Optional<CustomItem> customItem = customRepository.findCustomItemById(Integer.valueOf(item_id));
             Optional<HaveItem> newhaveItemOptional = haveItemRepository.findByCustomItemAndUser(customItem.get(), user); //TODO: 예외처리 추가하기
             HaveItem newhaveItem = newhaveItemOptional.get();
-            List<HaveItem> oldhaveItemList = haveItemRepository.findByUserAndWearing(user, true);
-            for(int i=0; i<oldhaveItemList.size(); i++){ //'for' loop can be replaced with enhanced 'for'
-                HaveItem oldhaveItem = oldhaveItemList.get(i);
-                //입으려는 아이템 타입과 같은 아이템 타입만 false로 해준다.
-                if(newhaveItem.getCustomItem().getItemType().equals(oldhaveItem.getCustomItem().getItemType())){
-                    oldhaveItem.updateHaveItemWearing(false);
-                    haveItemRepository.save(oldhaveItem);
-                }
-            }
             newhaveItem.updateHaveItemWearing(true);
             haveItemRepository.save(newhaveItem);
         }
-//        Optional<CustomItem> customItem = customRepository.findCustomItemById(item_id);
-//        Optional<HaveItem> newhaveItemOptional = haveItemRepository.findByCustomItemAndUser(customItem.get(), user); //TODO: 예외처리 추가하기
-//        HaveItem newhaveItem = newhaveItemOptional.get();
-//
-////        Optional<HaveItem> oldhaveItemOptional = user.getHaveItems().stream() //TODO: 예외처리 추가하기(커스텀 예외처리)
-////                .filter(HaveItem::isWearing)
-////                .reduce((a,b) -> {
-////                    try {
-////                        throw new Exception("캐릭터변경오류발생");
-////                    } catch (Exception e) {
-////                        throw new RuntimeException(e);
-////                    }
-////                });
-////        HaveItem oldhaveItem = oldhaveItemOptional.get();
-//        //TODO: 아이템 타입 보고 같은 타입 아이템 wearing을 false로 해야함
-//        List<HaveItem> oldhaveItemList = haveItemRepository.findByUserAndWearing(user, true);
-//        for(int i=0; i<oldhaveItemList.size(); i++){ //'for' loop can be replaced with enhanced 'for'
-//            HaveItem oldhaveItem = oldhaveItemList.get(i);
-//            //입으려는 아이템 타입과 같은 아이템 타입만 false로 해준다.
-//            if(newhaveItem.getCustomItem().getItemType().equals(oldhaveItem.getCustomItem().getItemType())){
-//                oldhaveItem.updateHaveItemWearing(false);
-//                haveItemRepository.save(oldhaveItem);
-//            }
-//        }
-//
-//        newhaveItem.updateHaveItemWearing(true);
-////        oldhaveItem.updateHaveItemWearing(false);
-//
-//        haveItemRepository.save(newhaveItem);
-//        haveItemRepository.save(oldhaveItem);
     }
 
     public void resetCharcter(User user){
@@ -129,9 +99,16 @@ public class CustomService {
     public void buyItem(User user, int item_id){
         Optional<CustomItem> customItemOptional = customRepository.findCustomItemById(item_id);
         CustomItem customItem = customItemOptional.get();
-        if(haveItemRepository.findByCustomItemAndUser(customItem, user).isPresent()){
-           throw new IllegalArgumentException("이미 결제된, 가지고 있는 아이템입니다."); //TODO: 커스텀 예외처리
-        }
+        //haveItemRepository.findByCustomItemAndUser(customItem, user).orElseThrow(() -> new BuyOwnedItemException(ErrorType.BUY_OWNED_ITEM_ERROR.getMessage()));
+        haveItemRepository.findByCustomItemAndUser(customItem, user).ifPresent(item -> {
+            throw new BuyOwnedItemException(ErrorType.BUY_OWNED_ITEM_ERROR.getMessage());
+        });
+//        Optional<HaveItem> ha = haveItemRepository.findByCustomItemAndUser(customItem, user);
+//        if(haveItemRepository.findByCustomItemAndUser(customItem, user).isPresent()){
+//           //throw new IllegalArgumentException("이미 결제된, 가지고 있는 아이템입니다."); //TODO: 커스텀 예외처리
+//            System.out.println("sdjlkfa");
+//            throw new BuyOwnedItemException(ErrorType.BUY_OWNED_ITEM_ERROR.getMessage());
+//        }
         //TODO: 결제창으로 넘어가서 아이템 결제하는 부분 구현하기
         haveItemRepository.save(new HaveItem(user, customItem));
     }
