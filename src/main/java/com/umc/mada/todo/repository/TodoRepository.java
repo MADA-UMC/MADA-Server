7f19ab45d195b3610c140a69d0ee1295b872d7c8
yujiyea <54650556+yujiyea@users.noreply.github.com>
2023-09-04 23:48:43 +0900

    Update : 주,월별 통계 동적쿼리로 수정하기

--- src/main/java/com/umc/mada/todo/repository/TodoRepository.java
+++ src/main/java/com/umc/mada/todo/repository/TodoRepository.java
@@ -35,13 +35,13 @@ public interface TodoRepository extends JpaRepository<Todo, Integer> {
 //            "    FROM TODO, (SELECT @num\\:=-1) num\n" +
 //            "    LIMIT 31) B on A.date = B.date\n" + //TODO: limit 진짜 한달 날짜로 해야함
 //            "group by month(B.date);", nativeQuery = true)
-    @Modifying
-    @Transactional
+//    @Modifying
+//    @Transactional
     @Query(value = "select IFNULL(ROUND(AVG(A.complete)*100,1),0) as completeTodoPercent, ROUND(COUNT(A.complete)/COUNT(*),1) as todosPercent\n" +
         "from (select user_id, T.date, T.complete\n" +
         "      from TODO T\n" +
-        "      where (user_id = 55) and (T.date between '2023-08-01' and '2023-08-31') and `repeat` = 'N') A right join\n" +
-        "    (SELECT DATE_FORMAT(ADDDATE('2023-08-01', INTERVAL @num\\:=@num+1 DAY), '%Y-%m-%d') `date`\n" +
+        "      where (user_id = :uid) and (T.date between :startDate and :endDate) and `repeat` = 'N') A right join\n" +
+        "    (SELECT DATE_FORMAT(ADDDATE(:startDate, INTERVAL @num\\:=@num+1 DAY), '%Y-%m-%d') `date`\n" +
         "    FROM TODO, (SELECT @num\\:=-1) num\n" +
         "    LIMIT 31) B on A.date = B.date\n" + //TODO: limit 진짜 한달 날짜로 해야함
         "group by month(B.date);", nativeQuery = true)
@@ -53,7 +53,7 @@ public interface TodoRepository extends JpaRepository<Todo, Integer> {
 //            "    FROM TODO, (SELECT @num\\:=-1) num\n" +
 //            "    LIMIT 31) B on A.date = B.date\n" + //TODO: limit 진짜 한달 날짜로 해야함
 //            "group by month(B.date);", nativeQuery = true)
-    List<StatisticsVO> findTodosMonthAVG(@Param("uid") Long uid,@Param("endDate") LocalDate endDate, @Param("startDate") LocalDate startDate);
+    List<StatisticsVO> findTodosMonthAVG(@Param("uid")Long uid,@Param("endDate") LocalDate endDate, @Param("startDate") LocalDate startDate);
 
 //    @Query(value = "select IFNULL(ROUND(AVG(A.complete)*100,1),0) as completeTodoPercent, ROUND(COUNT(A.complete)/COUNT(*),1) as todosPercent\n" +
 //            "from (select user_id, T.date, T.complete\n" +
@@ -73,5 +73,7 @@ public interface TodoRepository extends JpaRepository<Todo, Integer> {
             ") B on A.date = B.date\n" +
             "group by week(B.date);", nativeQuery = true)
     List<StatisticsVO> findTodosWeekAVG(@Param("uid") Long uid,@Param("endDate") LocalDate endDate, @Param("startDate") LocalDate startDate);
+
+//    List<StatisticsVO>
 
 }
