e8a0e96d225dc83ddc61ceea4f741dba8c8f2cea
yujiyea <54650556+yujiyea@users.noreply.github.com>
2023-09-07 19:46:22 +0900

    Feat : 주,월별 투두 통계 수정 완료 & 카테고리 통계 기능 개발
    - 카테고리 통계: Dto, VO 생성, repository findCategoryAVG 생성,
    - 주, 월별 통계: 쿼리 수정하여 값이 있지만 0이 나오는 경우 수정,중복되는 코드들 통일하기

--- src/main/java/com/umc/mada/todo/dto/TodoStatisticsResponseDto.java
+++ src/main/java/com/umc/mada/todo/dto/TodoStatisticsResponseDto.java
@@ -1,23 +1,35 @@
 package com.umc.mada.todo.dto;
 
+import com.umc.mada.todo.repository.CategoryStatisticsVO;
 import lombok.Builder;
 import lombok.Getter;
 
+import java.util.List;
+import java.util.stream.Collectors;
+
 @Getter
 public class TodoStatisticsResponseDto {
-    Double todosPercent;
-    Double completeTodoPercent;
+    private final String nickName;
+    private final Double todosPercent;
+    private final Double completeTodoPercent;
+    private final List<CategoryStatisticsDto> categoryStatistics;
 
     @Builder
-    public TodoStatisticsResponseDto(Double todosPercent, Double completeTodoPercent){
+    public TodoStatisticsResponseDto(String nickName, Double todosPercent, Double completeTodoPercent, List<CategoryStatisticsDto> categoryStatistics){
+        this.nickName = nickName;
         this.todosPercent = todosPercent;
         this.completeTodoPercent = completeTodoPercent;
+        this.categoryStatistics = categoryStatistics;
     }
 
-    public static TodoStatisticsResponseDto of(Double todosPercent, Double completeTodoPercent){
+    public static TodoStatisticsResponseDto of(String nickName,Double todosPercent, Double completeTodoPercent, List<CategoryStatisticsVO> categoryStatistics){
         return TodoStatisticsResponseDto.builder()
+                .nickName(nickName)
                 .todosPercent(todosPercent)
                 .completeTodoPercent(completeTodoPercent)
+                .categoryStatistics(categoryStatistics.stream()
+                        .map(i -> CategoryStatisticsDto.of(i.getCategoryName(),i.getColor(), i.getRate()))
+                        .collect(Collectors.toList()))
                 .build();
     }
 }
