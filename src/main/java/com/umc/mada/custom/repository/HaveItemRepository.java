346ca4a085335aef311e28e069473391bfbd6a38
강지훈 <clone0922@gmail.com>
2024-05-08 15:23:16 +0900

    Feat : User Subscribe -> User VIP Custom append Service

--- src/main/java/com/umc/mada/custom/repository/HaveItemRepository.java
+++ src/main/java/com/umc/mada/custom/repository/HaveItemRepository.java
@@ -19,6 +19,7 @@ public interface HaveItemRepository extends JpaRepository<HaveItem, Long> {
     List<HaveItem> findByUserAndWearing(User user, boolean wearing);
     boolean existsByCustomItemAndUser(CustomItem customItem, User user);
 
+    Boolean deleteByUserAndCustomItem(User user, CustomItem customItem);
     @Query("select h.customItem from HaveItem h where h.user = :user and h.wearing = :wearing")
     List<CustomItem> findCustomItemByUserAndWearing(@Param("user") User user, @Param("wearing") boolean wearing);
 }
