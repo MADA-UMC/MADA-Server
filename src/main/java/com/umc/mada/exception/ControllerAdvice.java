84ef4b3f34e5a2c1eb0f8f8e62379ceb5dc5841b
yujiyea <yje3002@naver.com>
2024-10-17 22:46:34 +0900

    refactor: 주소 수정, 에러 코드 추가

--- src/main/java/com/umc/mada/exception/ControllerAdvice.java
+++ src/main/java/com/umc/mada/exception/ControllerAdvice.java
@@ -33,6 +33,11 @@ public class ControllerAdvice {
         return ResponseEntity.status(HttpStatus.BAD_REQUEST).body(errorResponse);
     }
 
+    @ExceptionHandler(DuplicationItemException.class)
+    public ResponseEntity<ErrorResponse> duplicateItemHandler(final DuplicationItemException e){
+        return ResponseEntity.status(HttpStatus.CONFLICT).body(new ErrorResponse(e.getMessage()));
+    }
+
 //    @ExceptionHandler(Exception.class)
 //    private ResponseEntity<ErrorResponse> unExpectedException(final Exception e){ //TODO: final HttpServletRequest request이거 추가해서 에러로그를 띄우기
 //        return ResponseEntity.internalServerError().body(new ErrorResponse("예상치 못한 서버 에러가 발생했습니다."));
